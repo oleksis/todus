1c8ece09e49f844175c65203a813697c926531c3
oleksis <oleksis.fraga@gmail.com>
2021-07-15 20:50:46 -0400

    Add test for s3: test_get_socket, test_parse_token

--- tests/test_util.py
+++ tests/test_util.py
@@ -16,14 +16,15 @@ class TestUtil(TestCase):
     def test_generate_token(self, mock_random_choice: MagicMock):
         lenght: int = 150
         expected = (
-            "XSvRz21yqf0KiMZEIiijWZiZWJKZCzPwR6RPiaYfIfJ5Wj0l"
-            "VXcT9gIynyQmDstQ5gce5uJ4Lm8lLzaaEjRdeqsRGVQ3QeT6"
-            "Rr2nntaqIr8TiQZHDyP8BgXgD7qO0ZTsshznVG08MrdbiK42ldUxJj"
+            "XSvRz21yqf0KiMZEIiijWZiZWJKZCzPwR6RPiaYfIfJ5Wj0lVX"
+            "cT9gIynyQmDstQ5gce5uJ4Lm8lLzaaEjRdeqsRGVQ3QeT6Rr2n"
+            "ntaqIr8TiQZHDyP8BgXgD7qO0ZTsshznVG08MrdbiK42ldUxJj"
         )
 
         mock_random_choice.side_effect = (ch for ch in expected)
         result = generate_token(150)
 
         mock_random_choice.assert_called()
+        self.assertEqual(mock_random_choice.call_count, 150)
         self.assertEqual(result, expected)
         self.assertEqual(len(result), len(expected))
